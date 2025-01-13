e440a1a8051757c84c03c08de162f0272a252b8e
Asantae <asantaems@gmail.com>
2025-01-13 09:27:26 -0500

    reorganizing code

--- Controllers/ExpenseController.cs
+++ Controllers/ExpenseController.cs
@@ -8,7 +8,9 @@ namespace ExpenseTrackerBackend.Controllers
     [ApiController]
     public class ExpenseController : ControllerBase
     {
-        private string connectionString = "Data Source=C:\\Users\\Asantae\\ExpenseTrackerBackend\\expense_tracker.db;Version=3;";
+        private string connectionString = @"DataSource=C:\Users\Asantae\Desktop\Dev\ExpenseTrackerBackend\expense_tracker.db;Version=3;";
+
+        // "C:\Users\Asantae\Desktop\Dev\ExpenseTrackerBackend\expense_tracker.db;Version=3;"
 
         [HttpGet("testConnection")]
         public IActionResult TestConnection()
@@ -79,6 +81,34 @@ namespace ExpenseTrackerBackend.Controllers
                 connection.Close();
                 return Ok(expense);
             }
+        }
+
+        [HttpGet("checkTables")]
+        public IActionResult CheckTables()
+        {
+        try
+        {
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT name FROM sqlite_master WHERE type='table'";
+                var command = new SQLiteCommand(query, connection);
+                var reader = command.ExecuteReader();
+
+                var tables = new List<string>();
+                while (reader.Read())
+                {
+                    tables.Add(reader["name"].ToString());
+                }
+
+                connection.Close();
+                return Ok(tables);
+            }
+        }
+        catch (SQLiteException ex)
+        {
+            return StatusCode(500, $"Error checking tables: {ex.Message}");
+        }
         }
     }
 }
